4c25d617a3eeadc7b548688026422300a06e8fdc
Jochen van Wylick <jowylick@microsoft.com>
2020-11-11 21:05:42 +0100

    Code commenting

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
+# Executes deployment script in /infra folder
+# Reason for doing it like this is so that 
+# - I can execute this file in my CD agent as 'regular' bash script
+# - I keep the benefits of code completion etc. from VS Code for the .azcli file format
+
 bash -c "../infra/deploy-infra.azcli"
