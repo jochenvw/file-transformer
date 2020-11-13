b86180ad4834b32ae0e4781990cca1fabe64071d
Jochen van Wylick <jowylick@microsoft.com>
2020-11-13 16:10:35 +0100

    Azure Function to Storage account auth with MSI Fixes #1

---
+++ app/BlobFileParameters.cs
@@ -0,0 +1,19 @@
+﻿namespace app
+{
+    public static partial class FileTransformer
+    {
+        public class BlobFileParameters
+        {
+            public string FileName { get; private set; }
+            public string BlobEndpoint { get; private set; }
+            public string BlobContainer { get; private set; }
+
+            public BlobFileParameters(string fileName, string blobEndpoint, string blobContainer)
+            {
+                FileName = fileName;
+                BlobEndpoint = blobEndpoint;
+                BlobContainer = blobContainer;
+            }
+        }
+    }
+}
